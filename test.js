3e4d13dd3be6515c2cdd0739aafe66f6597c01bd
Dan Allison <daniel.j.allison@gmail.com>
2017-01-26 12:34:34 -0800

    Change variable name

--- test.js
+++ test.js
@@ -30,8 +30,8 @@ describe('basic functionality', () => {
   })
 
   it('returns json', () => {
-    let doc = calculist.new(listData)
-    expect(doc.toJSON()).to.be.an('object')
+    let list = calculist.new(listData)
+    expect(list.toJSON()).to.be.an('object')
   })
 
 })
@@ -39,45 +39,45 @@ describe('basic functionality', () => {
 describe('calculation', () => {
 
   it('calculates plain values correctly', () => {
-    let doc = calculist.new({ text: 'foo [:] 1 + 1' })
-    expect(doc.valueOf()).to.eq('1 + 1')
+    let list = calculist.new({ text: 'foo [:] 1 + 1' })
+    expect(list.valueOf()).to.eq('1 + 1')
   })
 
   it('calculates plain expressions correctly', () => {
-    let doc = calculist.new({ text: 'foo [=] 1 + 1' })
-    expect(doc.valueOf()).to.eq(2)
+    let list = calculist.new({ text: 'foo [=] 1 + 1' })
+    expect(list.valueOf()).to.eq(2)
   })
 
   it('calculates functions correctly', () => {
-    let doc = calculist.new({ text: 'foo [=>] a, b | a + b' })
-    expect(doc.valueOf()(1, 1)).to.eq(2)
+    let list = calculist.new({ text: 'foo [=>] a, b | a + b' })
+    expect(list.valueOf()(1, 1)).to.eq(2)
   })
 
   it('references variables correctly', () => {
-    let doc = calculist.new({
+    let list = calculist.new({
       text: 'foo bar [=] 5',
       items: [
         {text: 'baz [=] 2'},
         {text: '[=] foo_bar - baz'}
       ]
     })
-    expect(doc.$item(1).valueOf()).to.eq(5 - 2)
+    expect(list.$item(1).valueOf()).to.eq(5 - 2)
   })
 
   it('references `$name` correctly', () => {
-    let doc = calculist.new({ text: 'foo [=] $name + 1' })
-    expect(doc.valueOf()).to.eq('foo1')
+    let list = calculist.new({ text: 'foo [=] $name + 1' })
+    expect(list.valueOf()).to.eq('foo1')
   })
 
   it('calculates `sum($items)` correctly', () => {
-    let doc = calculist.new({
+    let list = calculist.new({
       text: 'foo [=] sum($items)',
       items: [
         {text: '[:] 1'},
         {text: '[:] 1'}
       ]
     })
-    expect(doc.valueOf()).to.eq(2)
+    expect(list.valueOf()).to.eq(2)
   })
 
 })
